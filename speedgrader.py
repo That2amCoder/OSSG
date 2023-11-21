a6232b1b80d73d3d192ba31c8d8f4a9f2106cc44
Philip C <p.constantinou@student.vu.nl>
2023-11-21 01:33:10 +0100

    Added CG diff + autograde

--- speedgrader.py
+++ speedgrader.py
@@ -3,10 +3,16 @@ import os
 from getpass import getpass
 import requests
 import rich
+import csv
+import subprocess
+import re
+import difflib
 
-def write_into_temp(content):
-    with open("temp.txt", "a") as f:
-        f.write(content)
+def write_to_csv(vunetid, name, codegrade_grade, new_grade, comment):
+    with open("grades.csv", "a") as f:
+        writer = csv.writer(f)
+        writer.writerow([vunetid, name, codegrade_grade, new_grade, comment])
+
 
 def download_asg_setup():
     ghrepo = input("Enter the github repo of the framework: ")
@@ -27,17 +33,45 @@ if not os.path.exists("sandbox"):
     print("Then you need to set a codegrade password at:")
     rich.print("https://vu.codegra.de/forgot/2f5725ae-4aac-461c-85f5-751ed7ca1342")
     os.mkdir("sandbox")
+    print("NOTE 2: You will need bitbucket credentials to download the framework!")
     input("Press enter to continue")
     open("temp.txt", "w").close()
     download_asg_setup()
+
+    print("--- Do you want to save your codegrade password? ---")
+    #print NOTE: !THEY WILL BE STORED IN PLAINTEXT!") in red
+    rich.print("[red]NOTE: THEY WILL BE STORED IN PLAINTEXT!")
+
+    if (input("Enter y or n: ") == "y"):
+        # Create a creds.txt file
+        with open("creds.txt", "w") as f:
+            f.write("")
+
     print("Done!")
 
+usr = ""
+pwd = ""
+# Check if there is a creds.txt file in the current directory
+if os.path.exists("creds.txt"):
+    # If its empty then ask the user for their codegrade credentials
+    if os.stat("creds.txt").st_size == 0:
+        usr = input("Enter your vunet username: ")
+        pwd = getpass("Enter your vunet password: ")
+        # Write the credentials to the creds.txt file
+        with open("creds.txt", "w") as f:
+            f.write(usr + "\n")
+            f.write(pwd)
+    # If its not empty then read the credentials from the file
+    else:
+        with open("creds.txt", "r") as f:
+            usr = f.readline().strip()
+            pwd = f.readline().strip()
+else:
+    usr = input("Enter your vunet username: ")
+    pwd = getpass("Enter your vunet password: ")
 
 
 
-print("")
-usr = input("Enter your vunet username: ")
-pwd = getpass("Enter your vunet password: ")
 namedict = {}
 frameworkpath = "./sandbox/framework"
 
@@ -49,7 +83,7 @@ with codegrade.login(
 ) as client:
     print("Logged in as", client.user.get().name)
 
-    print("Do you want to restart the sandbox? (y/n):")
+    print("Do you want to change assignment? (y/n):")
     if input("Enter y or n: ") == "y":
         os.system("rm -rf ./sandbox")
         download_asg_setup()
@@ -76,17 +110,27 @@ with codegrade.login(
     print("Enter function: ")
     print("[1] - Grade all assigned submissions")
     print("[2] - Search for a specific student")
-    if input("Enter the number of the function you want to use: ") == "2":
+    print("[3] - AutoGrade everything and output to csv (will take a while)")
+
+    option = input("Enter the number of the function you want to use: ")
+    if option == "2":
         search = input("Enter the vu-net id of the student you want to grade: ")
         submissions = [x for x in submissions if search in x.user.username]
-    else:
+    elif option == "1":
         try:
+            # This assumes everyone is assigned to a grader
             submissions = [x for x in submissions if x.assignee.name == client.user.get().name]
         except:
             print("You are not assigned to grade any submissions for this assignment. Bye!")
             exit()
 
+    autograde = option == "3"
     print("Submissions to grade: ", len(submissions))
+    # Initialise the csv file
+    with open("grades.csv", "w") as f:
+        writer = csv.writer(f)
+        writer.writerow(["VUNETID", "NAME", "CODEGRADE GRADE", "NEW GRADE", "COMMENT"])
+
     for submission in submissions:
         if submission is None:
             continue
@@ -94,57 +138,109 @@ with codegrade.login(
         os.system("cls" if os.name == "nt" else "clear")
             
         print("Submission by", submission.user.name)
-        # Download the submission
+        # get the autotest output using the codegrade api
+        auttest = client.auto_test.get(auto_test_id=assignment.auto_test_id)
+        submres = client.auto_test.get_result_by_submission(auto_test_id=assignment.auto_test_id, submission_id=submission.id, run_id=auttest.runs[0].id)
+        codegradeoutput = submres.step_results[0].log.haystack.value
+
+        # Get the submission url
         subm = client.submission.get(submission_id=submission.id,type="zip")
         res = subm.url
 
-        #Download the submission from the url
+        # Download the submission from the url
         r = requests.get(res, allow_redirects=True)
         strid = str(submission.user.id)
         open("sandbox/"+strid+".zip", 'wb').write(r.content)
 
-        #unzip the submission
+        # unzip the submission
         os.system("unzip ./sandbox/"+strid+".zip -d ./sandbox/"+strid)
-        #Delete the zip file
+        # Delete the zip file
         os.system("rm ./sandbox/"+strid+".zip")
 
-        #Read the shell.c file
-        #get the directories in the sandbox
+        # Get the directories in the sandbox
         dir = "./sandbox/"+strid+"/"
-        dir += os.listdir(dir)[0] + "/" # Its either /top/ or /shell asg/ so get whatever is submitted
+        dir += os.listdir(dir)[0] + "/" # Its either /top/ or /asg name/ 
         dirs = os.listdir(dir)
+
         # If there are no .c files, then it is a tar.gz file
         if not any(x.endswith(".c") for x in dirs):
             # Get the .tar.gz file name
             tarfile = [x for x in dirs if x.endswith(".tar.gz")][0]
             dirs += os.listdir("./sandbox/"+strid+"/"+tarfile+"/" +dirs[0])
 
-        #get the directory that contains the shell.c file
+        # Get the directory that contains the assignment .c file
         dirpath = dir
         dir += [x for x in dirs if x.startswith(subname+".c")][0]
-        #Get the path of the last folder
-        #Print the contents of the file using rich
+        # TODO: If there are multiple c files included, it makes our life harder so I'll fix that later
+        
+        # Get the path of the last folder
+        # Print the contents of the file using rich
         rich.print(open(dir).read())
         if "/top/" in dir:
             os.system("cp " + dir + " " + frameworkpath + "/"+subname+".c")
             dirpath = frameworkpath
-        #Ask the grader if the submission is correct
-        correct = input("Should this submission be run? (y/n): ")
-        if correct == "y":
-            #Run "make docker-check" and get the output
-            os.system("sudo make docker-check -C "+dirpath+"/")
-            #Ask the grader if the submission is correct
-            print("CODEGRADE GRADE: ", submission.grade)
-            correct = input("Is this submission correct? (y/n): ")
-            if correct == "y":
-                namedict[submission.user.name] = str(submission.grade)
+
+        # Ask the grader if the submission is malware
+        correct = "n"
+        if not autograde:
+            correct = input("Should this submission be run? (y/n): ")
+        if correct == "y" or autograde:
+
+            # Run "make docker-check" and get the output
+            output = subprocess.check_output(["make", "docker-check"], cwd=dirpath, universal_newlines=True)
+            grade = re.search("Executed all tests, got (.*)\/", output).group(1)
+            # Remove whitespace
+            grade = grade.replace(" ", "")
+            try:
+                grade = float(grade)
+            except:
+                grade = 0.0
+            # Ask the grader if the submission is correct
+            discrepancy = submission.grade != grade
+            if not discrepancy:
+                print("GRADE: ", submission.grade)
             else:
-                newgrade = input("Enter the new grade: ")
-                newcomment = input("Reason for the new grade: ")
-                namedict[submission.user.name] = newgrade + " | " + newcomment
-            write_into_temp(submission.user.name + " | " + str(namedict[submission.user.name]) + "\n")
+                # Remove all color codes from the codegrade output
+                plaintextoutput = re.sub(r"\x1b\[[0-9;]*m", "", output)
+
+                # Get the diff between the codegrade output and the local output
+                diff = difflib.Differ().compare(codegradeoutput.splitlines(), plaintextoutput.splitlines())
+
+                # Print legend of the diff output
+                print("LEGEND:")
+                # - is codegrade output (red)
+                rich.print("[red]- CODEGRADE OUTPUT")
+                # + is local output (green)
+                rich.print("[green]+ LOCAL OUTPUT")
+                rich.print("-----------------------------------")
+                for line in diff:
+                    # If there is a + or - in the line, then print it in the respective color
+                    if line.startswith("+"):
+                        rich.print('[green]'+line)
+                    elif line.startswith("-"):
+                        rich.print('[red]'+line)
+                    else:
+                        rich.print(line)
+
+                rich.print("DISCREPANCY!\n [red] CODEGRADE: ", submission.grade, " | [green] LOCALGRADE: ", grade)
+
+            grade = "NA"
+            newgrade = "NA"
+            newcomment = ""
+            if discrepancy:
+                newcomment = "GDIF: " + str(grade - submission.grade) + "\n"
+            if not autograde or discrepancy:
+                change = input("Override Grade? (y/N): ")
+                if change == "y":
+                    newgrade = input("Enter the new grade: ")
+                    newcomment = input("Reason for the new grade: ")
+                else:
+                    grade = str(submission.grade)
+                    newgrade = grade
+
+
+            write_to_csv(submission.user.username, submission.user.name, submission.grade, grade, newcomment)
+        # Clean up the sandbox
+        os.system("rm -rf ./sandbox/"+strid)
 
     print("Done!")
-    with open("grades.txt", "w") as f:
-        for key, value in namedict.items():
-            f.write("%s:,%s \n" % (key, value))
